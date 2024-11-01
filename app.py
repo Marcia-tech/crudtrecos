5715241e645acd49456318ee4477e9f098fbd388
Marcia-tech <marcia.martina@gmail.com>
2024-11-01 11:46:43 -0300

    01nov

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+# Importa as dependências
+from flask import Flask, render_template
 
+# Criar um aplicativo chamado 'app' do tipo 'Flask'
 app = Flask(__name__)
 
+# Processa a rota "raiz" executando a função logo abaixo
 @app.route("/")
-def hello_world():
-    return "<p>Olá Mundo!</p>"
-
+def home(): #função a ser executada
+    return render_template('_template.html') # retorno para o navegador
+    page = {
+        'titulo': "JocaTrecos'"
+    }
+    # retorno para o navgador
+    return render_template('_template.html', **page)
+# Executa o servidor de teste
 if __name__ == '__main__':
     app.run(debug=True)
